63c60d1463ea8a57056a508088cd623526242f62
ChintanIdentified <chintan@identified.com>
2012-07-24 09:25:05 -0700

    Renamed index.js to search.js

---
+++ src/main/webapp/js/search.js
@@ -0,0 +1,124 @@
+
+var Search = function() {
+
+};
+
+Search.prototype = {
+  init : function() {
+    this.initUI();
+    this.initResultsPane();
+  },
+  initUI : function() {
+    this.initSearchButton();
+    this.initSearchAutoComplete();
+  },
+  initResultsPane : function() {
+    this.searchPane = new SearchPane();
+    this.searchPane.init();
+  },
+  validate : function( value ) {
+    if( value.length == 0 ) {
+      return false;
+    }
+    return true;
+  },
+  search : function() {
+    var value = $.trim( $( ".searchfield input" ).val() );
+    if( ! this.validate( value ) ) {
+      return;
+    }
+    this.searchPane.search( value ); 
+  },
+  initSearchButton : function() {
+    var _this = this;
+    $("#searchbutton button").click( function() {
+      _this.search();
+    });
+  },
+  initSearchAutoComplete : function() {
+    var _this = this;
+ 
+    $( ".searchfield input" ).keypress( function( e ) {
+      $( ".searchfield input" ).css( "background-image", "none" );
+      if( e.keyCode == 13 ) {
+        $( this ).autocomplete( "close" );
+        _this.search(); 
+      }
+    });
+ 
+    $.getJSON( "rest/program/all", function( instruments ) {
+      for( var i in instruments ) {
+        instruments[i].value = instruments[i].name;
+        instruments[i].label = instruments[i].name;
+      }
+      $( ".searchfield input" ).autocomplete({
+        source: instruments,
+        minLength: 1,
+        select: function( event, ui ) {
+          $( ".searchfield input" ).val( ui.item.name );
+          /*$( ".searchfield input" ).css( "background-image",  "url( " + ui.item.image + " )" ); */
+          return false;
+        }
+      })
+      .data( "autocomplete" )._renderItem = function( ul, item ) {
+        return $( "<li></li>" )
+          .data( "item.autocomplete", item )
+          .append( "<a><div class='item-image'><img style='display:none;' src='" + item.image + "'/></div><div class='item-name'>" + item.name + "</div></a>" )
+          .appendTo( ul );
+      };
+    });
+  }
+};
+
+var SearchPane = function() {
+
+};
+
+SearchPane.prototype = {
+  init : function() {
+    this.initUI();
+  },
+  initUI : function() {
+  },
+  search: function( query ) {
+    var _this = this;
+    var c = new Date();
+    $.ajax( {
+      url: 'rest/school/search',
+      async: true,
+      contentType: 'application/json',
+      type: "POST",
+      dataType: "json",
+      context: _this,
+      data: JSON.stringify( 
+        { 
+          instrument: query
+        } ),
+      success: function( response, textStatus, jqXHR ){
+        var d = new Date()
+        var timeTaken = d.getTime() - c.getTime();
+        var timeInfo = timeTaken + " milliseconds";
+        if( timeTaken > 1000 ) {
+          timeInfo = ( timeTaken/1000 ) + " seconds";
+        }
+        $('#searchinfo').css('display', 'table-cell').html( "<span class='resultcount'>" + response.schools.length + "</span> schools found in " + timeInfo + "." );
+        for( var i in response.schools ) {
+          var school = response.schools[i];
+          school.domId = "school_" + school.musicSchoolId;
+          $("#searchResultSchoolTemplate").tmpl( school ).appendTo( "#searchresults")
+          for( var j in school.department ) {
+            $( "#searchResultDepartmentTemplate" ).tmpl( school.department[j] ).appendTo( "#searchresults #" + school.domId + " .department_list" );
+          }
+        }
+      },
+      error : function( jqXHR, textStatus, errorThrown ) {
+        console.log( errorThrown )
+      }
+    } );
+  }
+};
+
+var SearchResult = function() {
+  
+}
+
