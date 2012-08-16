2d4143880fb74319a31fc1a4691617c8874bac65
prarthan <schoolmusicacontact@gmail.com>
2012-08-16 14:00:20 -0700

    new ws to get styles

--- src/main/java/com/khatu/musicschool/dao/ProgramDao.java
+++ src/main/java/com/khatu/musicschool/dao/ProgramDao.java
@@ -2,11 +2,16 @@ package com.khatu.musicschool.dao;
 
 import java.util.List;
 
+import org.hibernate.criterion.Criterion;
+import org.hibernate.criterion.DetachedCriteria;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.orm.hibernate3.HibernateTemplate;
 import org.springframework.stereotype.Repository;
 
+import com.khatu.musicschool.model.Department;
 import com.khatu.musicschool.model.Program;
+import com.khatu.musicschool.model.Style;
 
 
 @Repository
@@ -19,5 +24,12 @@ public class ProgramDao {
 	@SuppressWarnings("unchecked")
 	public List<Program> getAllProgram(){
 		return (List<Program>)hibernateTemplate.loadAll(Program.class);
+	}
+	
+	public List<Style> getAllStylesById(List<Integer> instrumentIds){
+		DetachedCriteria query = DetachedCriteria.forClass(Style.class).add(Restrictions.in("programId", instrumentIds));
+		@SuppressWarnings("unchecked")
+		List<Style> styles = (List<Style>)this.hibernateTemplate.findByCriteria(query);
+		return styles;
 	}
 }
--- src/main/java/com/khatu/musicschool/wsresource/ProgramResource.java
+++ src/main/java/com/khatu/musicschool/wsresource/ProgramResource.java
@@ -1,22 +1,29 @@
 package com.khatu.musicschool.wsresource;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.khatu.musicschool.model.Program;
+import com.khatu.musicschool.model.Style;
 import com.khatu.musicschool.service.ProgramService;
 
 @Component
 @Path("/program")
 public class ProgramResource {
 	
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
 	
 	@Autowired
 	private ProgramService programService;
@@ -27,6 +34,25 @@ public class ProgramResource {
 	@Produces({MediaType.APPLICATION_JSON })
 	public List<Program> getAllProgram(){
 		return programService.getAllProgram();
+	}
+	
+	@GET
+	@Path("/style/{ids}")
+	@Produces({MediaType.APPLICATION_JSON })
+	public List<Style> getStyles(@PathParam("ids") String ids){
+		List<Style> styles = new ArrayList<Style>();
+		try{
+			List<Integer> instrumentIds = new ArrayList<Integer>();
+			List<String> instrumentIdstring = Arrays.asList(ids.split(","));
+			for(String currentId:instrumentIdstring){
+				instrumentIds.add(Integer.parseInt(currentId));
+			}
+			styles = programService.getAllStylesById(instrumentIds);
+		}catch(Exception e){
+			
+			logger.error("can not get styles",e.getStackTrace());
+		}
+		return styles;
 	}
 
 }
